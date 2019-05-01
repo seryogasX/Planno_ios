0750332be254762b4dd1840149f42227370cd7ac
e1nfalt <sergio.petrovx@yandex.ru>
2019-05-01 22:02:47 +0300

    login screen init

---
+++ Planno_ios/Planno_ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Planno_ios
+//
+//  Created by Сергей Петров on 01/05/2019.
+//  Copyright © 2019 SergioPetrovx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
